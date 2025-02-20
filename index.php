8100d8d2d65f3a9c4b7a8a89197c85404baa05e6
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2025-02-20 11:31:20 -0700

    Add resources to home page

--- index.php
+++ index.php
@@ -4,7 +4,47 @@
 
   <body>
     <h1><?php bloginfo('name'); ?></h1>
+    <p class="tagline">Conserving and restoring native habitat in Texas’s Big Bend.</p>
     <?php wp_footer(); ?>
+
+    <section id="about" class="about">
+      <p>
+        ReGreen Big Bend helps landowners in the 190,000-acre Terlingua Ranch
+        deploy native plants and water conservation techniques to restore desert land for
+        flora and fauna. We do so by example on our own land and by growing and
+        selling native plants.
+      </p>
+
+      <p>Our goal is to have fun doing something good for the land and creatures. It’s not
+        any big, organized deal. We’re making it up as we go along, making sure to
+        consult experts and not do anything harmful.</p>
+
+      <p>We work mostly in the winter because it just too hot the rest of the time.</p>
+
+      <p><a href="https://www.instagram.com/regreenbigbend/">@regreenbigbend</a> on Instagram, now and then.
+        Mostly then.</p>
+    </section>
+
+    <section id="resources" class="resources">
+      <h2>Resources</h2>
+      <ul>
+        <li><a href="https://www.npsot.org/chapters/big-bend/">Big Bend Chapter</a> of the Native Plant Society of Texas
+        </li>
+        <li><a href="https://www.ckwri.tamuk.edu/research-programs/texas-native-seeds-program-tns">Texas Native Seed
+            Program</a></li>
+        <li><a href="https://www.npsot.org">Native Plant Society of Texas</a></li>
+        <li><a href="https://www.cdri.org">Chihuahuan Desert Nature Center</a></li>
+        <li>Jim Martinez and Jim Fissel, and their book <a href="https://a.co/d/iON96BW"><cite>Marfa Garden</cite></a>
+        </li>
+        <li>Brad Lancaster and his <a
+            href="https://www.amazon.com/stores/Brad-Lancaster/author/B00E82SVUW?ref=ap_rdr&amp;isDramIntegrated=true&amp;shoppingPortalEnabled=true">books
+            on rainwater harvesting</a></li>
+        <li>Robert Pavlis, <a href="https://a.co/d/0FHXNu9"><cite>Soil Science for Gardeners</cite></a></li>
+        <li><a href="https://www.plantsofthesouthwest.com">Plants of the Southwest</a>, Santa Fe, NM</li>
+        <li><a href="http://southwestperennials.com">Southwest Perennials</a>, Dallas, TX</li>
+        <li><a href="https://bamertseed.com">Bamert Seed</a>, Muleshoe, TX</li>
+      </ul>
+    </section>
   </body>
 
 </html>
