0f70bf7a82615ab9de2ea74adb13d02f0d642546
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2025-02-22 13:01:41 -0700

    Style footer

--- footer.php
+++ footer.php
@@ -1,8 +1,9 @@
 <footer>
   <div class="site-title">ReGreen Big Bend</div>
   <ul class="links">
-    <li><a href="mailto:regreenbigbend@gmail.com">regreenbigbend@gmail.com</a></li>
-    <li><a href="https://www.instagram.com/regreenbigbend/" class="contact">@regreenbigbend</a></li>
+    <li><a href="mailto:regreenbigbend@gmail.com">Email</a></li>
+    <span>&nbsp;|&nbsp;</span>
+    <li><a href="https://www.instagram.com/regreenbigbend/">Instagram</a></li>
   </ul>
 </footer>
 
