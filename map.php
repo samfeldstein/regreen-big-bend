59fa123a382ae038cd437316c2b06f27d9e2e373
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2025-02-22 14:13:25 -0700

    Finish map page layout

--- map.php
+++ map.php
@@ -7,22 +7,21 @@ Template Name: Map
 <?php get_header(); ?>
 
 <header>
-  <h1>Brewster County, TX <br />Terlingua Ranch Property Appraisal Information</h1>
-  <p class="tagline">Conserving and restoring native habitat in Texas’s Big Bend.</p>
   <nav>
+    <a href="/">Home</a>
     <a href="/land/">Land for Sale</a>
   </nav>
+  <h1>Brewster County, TX <br />Terlingua Ranch Property Appraisal Information</h1>
 </header>
 
 <!-- Modal -->
 <div id="disclaimerModal" class="modal">
   <div class="modal-content">
-    <h2></h2>
     <p>
       This interactive map uses data publicly available from the Brewster County Appraisal District.
       It was accurate at the time of map creation in late 2024 and early 2025, but much of the data
       changes regularly.</p>
-    <p>Regreen Big Bend LLC does not warrant nor accepts any liability for the
+    <p>Regreen Big Bend, LLC does not warrant nor accepts any liability for the
       accuracy of this map. Users use the map at their own risk and are responsible for checking
       any information with the <a href="https://brewstercotad.org">Brewster County Appraisal District</a>.</p>
     <p>
