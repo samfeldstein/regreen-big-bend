4758b424faeee5330d131abf79d07f0ca5819098
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2025-02-22 14:19:34 -0700

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<header>
+  <h1>404 - Page Not Found</h1>
+</header>
+
+<main>
+  <p>It looks like this page doesn't exist. If you think this is a mistake, please email us at <a
+      href="mailto:regreenbigbend@gmail.com">regreenbigbend@gmail.com</a>. Otherwise, you can return to the <a
+      href="/">Home page</a>.</p>
+</main>
+
+<?php get_footer(); ?>
