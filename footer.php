798fe44639a055172f940c27869a94cc265471dd
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2025-02-24 18:00:39 -0700

    Replace footer links list with div
    Have that | in there which would have had to wrap in a <li> to make semantic. Felt weird.

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
 <footer>
   <div class="site-title">ReGreen Big Bend</div>
-  <ul class="links">
-    <li><a href="/contact/">Contact</a></li>
+  <div class="links">
+    <a href="/contact/">Contact</a>
     <span>&nbsp;|&nbsp;</span>
-    <li><a href="https://www.instagram.com/regreenbigbend/">Instagram</a></li>
-  </ul>
+    <a href="https://www.instagram.com/regreenbigbend/">Instagram</a>
+  </div>
 </footer>
 
 <?php wp_footer(); ?>
